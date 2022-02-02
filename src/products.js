0b68cb336e35b1528d2e53df9353cb00f5172f8f
Alex Martinez <j.alexandermartinez@Alexs-MacBook-Air.local>
2022-02-02 13:29:09 -0500

    MySQL with Express

---
+++ src/products.js
@@ -0,0 +1,59 @@
+const res = require('express/lib/response')
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+   
+        host: '34.67.146.233',
+        user: 'root',
+        password: 'BuildingSoFlo',
+        database: 'Commerce'
+    
+})
+
+// const query = `SELECT * FROM Products`
+
+// connection.query(query, (err, results, fields) => {
+
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log(results)
+
+// });
+
+const getAllProducts = async () =>{
+    
+    const query = `SELECT * FROM Products`
+    const [results, fields] = await connection.promise().query(query)
+
+    console.log(results)
+
+    return results
+}
+
+const createProducts = async (product) =>{
+
+    const insertQuery = `INSERT INTO Products (Description, SKU, UserId)
+    VALUES ('${product.description}', '${product.sku}', ${product.userId})`
+
+    const [results, fields] = await connection.promise().query(insertQuery)
+
+    console.log(results)
+    
+    return results
+}
+
+createProducts({
+    description: "Alexs new Product",
+    sku: "Alex1234",
+    userId: 1
+})
+
+getAllProducts()
+
+
+connection.end();
+
+
+
